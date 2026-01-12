1523d35d95e2ec61f0f08979edf67e92360efe8d
asifazam008 <asifazam789@gmail.com>
2026-01-12 16:48:36 +0530

    api added for find delete

--- order-service/src/main/java/com/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/order_service/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.order_service.controller;
 
 import com.order_service.entity.Order;
+import com.order_service.entity.OrderItem;
 import com.order_service.service.OrderService;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,10 +19,14 @@ public class OrderController {
         this.service = service;
     }
 
+    // ================= CREATE ORDER =================
+
     @PostMapping
     public Order create(@RequestParam String customerId) {
         return service.createOrder(customerId);
     }
+
+    // ================= ADD ITEM =================
 
     @PostMapping("/{id}/items")
     public Order addItem(@PathVariable UUID id,
@@ -30,8 +36,35 @@ public class OrderController {
         return service.addItem(id, productId, qty, price);
     }
 
+    // ================= PAY ORDER =================
+
     @PostMapping("/{id}/pay")
     public Order pay(@PathVariable UUID id) {
         return service.markPaid(id);
+    }
+
+    // ==========================
+// FIND ONE ORDER
+// ==========================
+    @PostMapping("/find/{orderId}")
+    public Order findOrder(@PathVariable UUID orderId) {
+        return service.findOrder(orderId);
+    }
+
+    // ==========================
+// FIND ALL ORDERS
+// ==========================
+    @PostMapping("/findAll")
+    public List<Order> findAllOrders() {
+        return service.findAllOrders();
+    }
+
+    // ==========================
+// DELETE ITEM FROM ORDER
+// ==========================
+    @PostMapping("/{orderId}/items/{itemId}/delete")
+    public Order deleteItem(@PathVariable UUID orderId,
+                            @PathVariable UUID itemId) {
+        return service.deleteItem(orderId, itemId);
     }
 }
